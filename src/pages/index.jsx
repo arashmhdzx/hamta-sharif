c715da91eb3d105128686fd0bf4e14779e07db40
arash mehdizadeh <arash.mehdiizadeh@gmail.com>
2023-10-14 11:33:13 +0330

    added gltf package to make export
    added scene to dashboard

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Layout from '../layout/layout'
+
+import { Canvas } from "@react-three/fiber"
+import { Model } from '../data/Scene'
+import { OrbitControls } from '@react-three/drei'
 
 const Index = () => {
     return (
         <Layout>
-            <div className='flex justify-center items-center h-full'>Index</div>
+            <div className='flex justify-center items-center h-full'>
+                <Canvas className=' '>
+                    <ambientLight />
+                    <OrbitControls />
+                    <Suspense fallback={null}>
+                        <Model />
+                    </Suspense>
+                </Canvas>
+            </div>
         </Layout>
     )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-
+    const navigate = useNavigate()
+    const [user, setUser] = useState("")
+    const [password, setPassword] = useState("")
     function onSubmitHandler (e){
         e.preventDefault();
-        localStorage.setItem("hamta-login-token",JSON.stringify({user:"admin",pass:"admin"}))
+        
+        if(user === "admin" || password ==="admin"){
+            alert("set fields to admin")
+        }
+        else{
+            localStorage.setItem("hamta-login-token",JSON.stringify({user:"admin",pass:"admin"}))
+            navigate("/dashboard")
+        }
+
     }
 
     return (
@@ -12,8 +23,8 @@ const Login = () => {
             <div className='flex flex-col bg-white rounded-xl items-center h-fit p-9'>
                 Login
                 <form onSubmit={onSubmitHandler} className='flex flex-col'>
-                    <input type="text" className='my-2 py-2 px-4 rounded bg-slate-200' placeholder='admin' />
-                    <input type="password" className='my-2 py-2 px-4 rounded bg-slate-200' placeholder='admin' />
+                    <input type="text" onChange={(e)=>setUser(e.target.value)} className='my-2 py-2 px-4 rounded bg-slate-200' placeholder='admin' />
+                    <input type="password" onChange={(e)=>setPassword(e.target.value)}  className='my-2 py-2 px-4 rounded bg-slate-200' placeholder='admin' />
                     <button type="submit" className='py-2 px-4 rounded-md text-white mt-6 bg-orange-500'>ثبت</button>
                 </form>
                 <div className='flex flex-col items-center'>
